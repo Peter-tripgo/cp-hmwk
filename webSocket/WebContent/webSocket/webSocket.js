acde0bb0ef2d308d65ed5214c98e07f97dfcf746
Peter-tripgo <peter.tripgo@gmail.com>
2022-03-26 21:46:18 +0900

    주석정리

--- webSocket/WebContent/webSocket/webSocket.js
+++ webSocket/WebContent/webSocket/webSocket.js
@@ -1,6 +1,5 @@
 var socket; // 소켓
 var market='"KRW-BTC","KRW-ETH","BTC-ETH","KRW-LTC","KRW-ZRX"';
-var arrMarket=["KRW-BTC","KRW-ETH","BTC-ETH","KRW-LTC","KRW-ZRX"];
 
 // 최종결과데이터
 var resultDataSet;
@@ -9,14 +8,17 @@ var resultDataSet;
 // 데이터 class
 class ResultDataSet {
 	constructor(){
-		console.log("ResultDataSet Constructor");
 		
 		// 웹소켓으로 가져온 호가 정보가 실시간으로 최종 데이터(위 예시 형태)에 반영돼야 합니다.
 		this.dataSet = {};
 
 	}
 	
-	// 최종 데이터(위 예시 형태)에 반영
+	/**
+	 *  최종 데이터(위 예시 형태)에 반영
+	 *  @param string market
+	 *  @param object orderbook_units
+	 */
 	update(market, orderbook_units){
 		
 		var orderBooks={};
@@ -24,19 +26,27 @@ class ResultDataSet {
 		this.dataSet[market] = orderBooks;
 	}
 	
-	// 현재 데이터 되돌려주자
+	/**
+	 * 현재 데이터 되돌려준다
+	 */
 	getData(){
 		return this.dataSet;
 	}
 	
-	// orderbook_units: Array(15) 로 
+	/**
+	 *  최종 데이터에 반영
+	 *  @param object arrOrderBookUnits
+	 */
 	getSortedAsksBidsObject(arrOrderBookUnits){
 		
 		var orderLength = arrOrderBookUnits.length;
+		if (orderLength == null || orderLength == 0) return;
 		
+		// 정열을 위해 배열로 선언
 		var arrAsks=[];
 		var arrBids=[];
 		
+		// Asks와 Bids를 Array로 만든다.
 		for (var i=0;i<orderLength;i++){
 			arrAsks.push({price:arrOrderBookUnits[i].ask_price, quantity:arrOrderBookUnits[i].ask_size});
 			arrBids.push({price:arrOrderBookUnits[i].bid_price, quantity:arrOrderBookUnits[i].bid_size});
@@ -50,10 +60,10 @@ class ResultDataSet {
 			  if (a.ask_price < b.ask_price) {
 			    return -1;
 			  }
-			  // a must be equal to b
+			  
 			  return 0;
 			});
-		
+		// asks는 price기준으로 오름차순, bids는 price기준으로 내림차순 정렬
 		arrBids.sort(function (a, b) {
 			  if (a.bids_price < b.bids_price) {
 				    return 1;
@@ -61,7 +71,7 @@ class ResultDataSet {
 				  if (a.bids_price > b.bids_price) {
 				    return -1;
 				  }
-				  // a must be equal to b
+				  
 				  return 0;
 				});
 		
@@ -69,23 +79,27 @@ class ResultDataSet {
 	}
 	
 }
+// 초기화
+resultDataSet = new ResultDataSet();
 
 
-// 웹소켓 연결
+/**
+ * 웹소켓 연결
+ * @returns
+ */
 function connectWS() {
 	if(socket != undefined){
 		console.log("Socket에 뭔가 있다! 그래서 다시 연결하지 않는다.");
 		return;
 	}
 	
-	resultDataSet = new ResultDataSet();
-	
 	socket = new WebSocket("wss://api.upbit.com/websocket/v1");
 	socket.binaryType = 'arraybuffer';
 
-	socket.onopen 	= function(e){ 
+	socket.onopen = function(e){ 
 		
-			//		 [{ticket field}, {type field}, {format field}] isOnlySnapshot isOnlyRealtime
+		    document.getElementById("log").value ="웹소켓 연결.\n데이터 수신중...";
+			// [{ticket field}, {type field}, {format field}] isOnlySnapshot isOnlyRealtime
 
 			var query = '[{"ticket":"UNIQUE_TICKET1234Featter082"},'+
 			            ' {"type":"orderbook","codes":[' + market +'], "isOnlyRealtime":true}' +
@@ -94,9 +108,17 @@ function connectWS() {
 		
 			socket.send(query);
 	}
-	socket.onclose 	= function(e){ 
-		socket = undefined; 
+	
+	socket.onerror = function(e){ 
+		document.getElementById("log").value ="웹소켓 연결 오류.";
+	    console.log("WebSocket Error: " , e);
 	}
+	
+	socket.onclose = function(e){ 
+		socket = undefined; 
+		document.getElementById("log").value ="웹소켓 해제.";
+	}
+	
 	socket.onmessage= function(e){ 
 		var enc = new TextDecoder("utf-8");
 		var arr = new Uint8Array(e.data);
@@ -109,7 +131,11 @@ function connectWS() {
 		
 	}	
 }
-// 웹소켓 연결 해제
+
+/**
+ * 웹소켓 연결 해제
+ * @returns
+ */
 function closeWS() {
 	if(socket != undefined){
 		socket.close();
@@ -118,17 +144,21 @@ function closeWS() {
 	}	
 }
 
-
-// 데이터 처리 (* 아래 예시 데이터 형태)
+/**
+ * 데이터 처리 (예시 데이터 형태)
+ * @param orderbook
+ * @returns
+ */
 function processMain(orderbook){
 
 	// 마켓코드
 	var market = orderbook.code;	
 	
-	// 수신받은 데이터에서 orderbook_unit을 처리한다.
+	// 수신받은 데이터에서 orderbook_unit을 처리해서 최종 resultDataSet 에 set한다
 	resultDataSet.update(market,orderbook.orderbook_units);
 	
-	console.log(resultDataSet.getData());
+	console.log(market + " : " + resultDataSet.getData());
+//	console.log(market + " : " + JSON.stringify( resultDataSet.getData()));
 }
 
 
